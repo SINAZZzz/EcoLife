17bc52e4ba5e77b13c5c52c0b7604439e27ead4b
Sina Zinsaz <101419921+SINAZZzz@users.noreply.github.com>
2024-06-15 20:31:41 +0330

    feat: navbar

---
+++ script.js
@@ -0,0 +1,12 @@
+var data_menu = ["Home" , "Shop" , "Blogs" , "About" , "Contact"];
+
+var menu = document.getElementById('menu');
+
+data_menu.map((item , index) => {
+    var element = document.createElement('p');
+    element.textContent = item;
+    if(index == 0){
+        element.classList.add('menu-item-first');
+    }
+    menu.appendChild(element)
+})
