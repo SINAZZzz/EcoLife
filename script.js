b551a90d920071cbc88050be7629a328c63e05e4
Sina Zinsaz <101419921+SINAZZzz@users.noreply.github.com>
2024-06-16 23:59:33 +0330

    feat: comments

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
+// var
 var data_menu = ["Home" , "Shop" , "Blogs" , "About" , "Contact"];
-
 var menu = document.getElementById('menu');
-
+// map
 data_menu.map((item , index) => {
     var element = document.createElement('p');
     element.textContent = item;
